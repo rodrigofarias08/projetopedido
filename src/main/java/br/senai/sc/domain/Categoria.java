647c177f3ccd4e432f766cee0e3dfd4b6a740ac5
Rodrigo Farias <rodrigofarias08@gmail.com>
2019-02-27 08:45:39 +0900

    Criado classe Categoria

---
+++ src/main/java/br/senai/sc/domain/Categoria.java
@@ -0,0 +1,36 @@
+package br.senai.sc.domain;
+
+public class Categoria {
+//Atributos:
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {
+		super();
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+}
