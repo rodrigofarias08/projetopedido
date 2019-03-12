0f6253b6d44b06d4ac671c590382b2195a2dabec
Rodrigo Farias <rodrigofarias08@gmail.com>
2019-03-12 09:25:27 -0300

    Criacao da classe Produto, mapeamentos JPA e testes de inclusão no banco de dados

--- src/main/java/br/senai/sc/ProjetoPedidoApplication.java
+++ src/main/java/br/senai/sc/ProjetoPedidoApplication.java
@@ -1,13 +1,38 @@
 package br.senai.sc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.senai.sc.domain.Categoria;
+import br.senai.sc.domain.Produto;
+import br.senai.sc.repositories.CategoriaRepository;
+import br.senai.sc.repositories.ProdutoRepository;
+
 @SpringBootApplication
-public class ProjetoPedidoApplication {
+public class ProjetoPedidoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoPedidoApplication.class, args);
+	}
+	
+	@Autowired
+	private CategoriaRepository repo;
+	@Autowired
+	private ProdutoRepository prod;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria i = new Categoria(null, "Informática");
+		Categoria e = new Categoria(null, "Escritório");
+		repo.save(i);
+		repo.save(e);
+		
+		Produto c = new Produto(null, "cadeira", 100.2);
+		Produto m = new Produto(null, "mesa", 200.3);
+		prod.save(c);
+		prod.save(m);
 	}
 
 }
--- src/main/java/br/senai/sc/domain/Produto.java
+++ src/main/java/br/senai/sc/domain/Produto.java
@@ -6,39 +6,47 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Categoria {
-//Atributos:
+public class Produto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
-	public Categoria() {
+	public Produto() {
 		super();
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	public Integer getId() {
 		return id;
 	}
-
+	
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
+	
 	public String getNome() {
 		return nome;
 	}
-
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 	
+	public Double getPreco() {
+		return preco;
+	}
 	
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
 	
 }
